d7523eade9a60f95ab6095a5e0b8b5ae923a54bb
jochenschmich <github@airsupply.de>
2017-08-16 14:46:29 +0200

    Added separator between the primary and secondary menu

--- viperpit-web/src/App.vue
+++ viperpit-web/src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div id="app">
     <router-link to="/maps">Maps</router-link> | Cockpits: <router-link to="/cockpits/f16">F-16</router-link> | <router-link to="/cockpits/f35">F-35</router-link>
+    <hr />
     <router-view></router-view>
 </div>
 </template>
