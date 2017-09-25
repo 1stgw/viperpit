95046da815027234053be1605e2a80801301fde9
jochenschmich <github@airsupply.de>
2017-09-25 11:18:58 +0200

    Added icons for basic roles

--- viperpit-web/src/components/f16/controls/Control.vue
+++ viperpit-web/src/components/f16/controls/Control.vue
@@ -1,8 +1,25 @@
-<template>
-<a type="button" :class="[{ active: isActive(id) }, 'btn ' + type + '-' + role + ' btn-block']" v-on:click="fireAction(id)">
-    {{label}}
-</a>
-</template>
+<template> <a type="button"
+    :class="[{ active: isActive(id) }, 'btn ' + type + '-' + role + ' btn-block']"
+    v-on:click="fireAction(id)">
+    <div v-if="role === 'up'">
+        <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+    </div>
+    <div v-else-if="role === 'down'">
+        <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
+    </div>
+    <div v-else-if="role === 'left'">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    </div>
+    <div v-else-if="role === 'right'">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    </div>
+    <div v-else-if="type === 'C'">
+        
+    </div>
+    <div v-else>
+        {{label}}
+    </div>
+</a> </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
