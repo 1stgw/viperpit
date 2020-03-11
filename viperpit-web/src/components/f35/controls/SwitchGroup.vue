25a50884e7c02a2c245d24364015b9ea43add5c6
jochenschmich <github@airsupply.de>
2020-03-11 12:11:36 +0100

    Restored the Switch Group.

---
+++ viperpit-web/src/components/f35/controls/SwitchGroup.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="switch-group">
+    <div align="center" style="padding: 0px;">
+      <strong>{{ label }}</strong>
+    </div>
+    <div align="center" style="padding: 0px;">
+      <div class="btn-group-vertical" role="group" :aria-label="description">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
