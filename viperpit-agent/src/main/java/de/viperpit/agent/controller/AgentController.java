477554ddfae6803853ab7783abe2cae563258bf5
jochenschmich <github@airsupply.de>
2020-06-15 10:52:20 +0200

    Moved state handling to StateProvider.

--- viperpit-agent/src/main/java/de/viperpit/agent/controller/AgentController.java
+++ viperpit-agent/src/main/java/de/viperpit/agent/controller/AgentController.java
@@ -2,11 +2,6 @@ package de.viperpit.agent.controller;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
-
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,9 +13,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
-import de.viperpit.agent.data.SharedMemoryStateProvider;
 import de.viperpit.agent.keys.KeyDispatcherService;
-import de.viperpit.agent.model.Agent;
 import de.viperpit.agent.model.InitializeStateEvent;
 import de.viperpit.agent.model.StateChangeEvent;
 import de.viperpit.agent.model.ToggleStateEvent;
@@ -37,13 +30,8 @@ public class AgentController implements ApplicationListener<ApplicationEvent> {
 
 	private static final String TOPIC_STATES_UPDATE = "/topic/cockpit/states/update";
 
-	private final Agent agent;
-
 	@Autowired
 	private KeyDispatcherService keyDispatcherService;
-
-	@Autowired
-	private SharedMemoryStateProvider sharedMemoryStateProvider;
 
 	@Autowired
 	private StateProvider stateProvider;
@@ -54,10 +42,6 @@ public class AgentController implements ApplicationListener<ApplicationEvent> {
 	@Value("${url}")
 	private String url;
 
-	public AgentController(@Value("#{environment.COMPUTERNAME}") String agentId) {
-		this.agent = new Agent(agentId);
-	}
-
 	@Override
 	public void onApplicationEvent(ApplicationEvent event) {
 		LOGGER.debug(event.toString());
@@ -67,11 +51,7 @@ public class AgentController implements ApplicationListener<ApplicationEvent> {
 	@SendTo(TOPIC_STATES_UPDATE)
 	public StateChangeEvent onStatesInit(InitializeStateEvent initializeStateEvent) {
 		LOGGER.info("State intialization requested.");
-		Map<String, Object> statesToUpdate = new HashMap<>();
-		for (Entry<StateConfiguration, Object> entry : stateProvider.getStates()) {
-			statesToUpdate.put(entry.getKey().getId(), entry.getValue());
-		}
-		return updateStates(statesToUpdate);
+		return stateProvider.initializeStates();
 	}
 
 	@MessageMapping(APP_STATES_TOGGLE)
@@ -80,55 +60,21 @@ public class AgentController implements ApplicationListener<ApplicationEvent> {
 		if (toggleStateEvent == null) {
 			return null;
 		}
-		String id = toggleStateEvent.getId();
-		StateConfiguration stateConfiguration = stateProvider.getStateConfiguration(id);
+		StateConfiguration stateConfiguration = stateProvider.getStateConfiguration(toggleStateEvent.getId());
 		if (stateConfiguration == null) {
 			return null;
 		}
 		keyDispatcherService.fire(stateConfiguration.getCallback());
-		if (stateConfiguration.isStateful()) {
-			Map<String, Object> statesToUpdate = new HashMap<>();
-			statesToUpdate.put(id, true);
-			for (String idOfRelated : stateConfiguration.getRelatedStateConfigurations()) {
-				statesToUpdate.put(idOfRelated, false);
-			}
-			return updateStates(statesToUpdate);
-		} else {
-			return null;
-		}
+		return stateProvider.toggleBooleanState(stateConfiguration);
 	}
 
 	@Scheduled(fixedRate = 1000)
 	public void onStatesUpdate() {
-		Map<String, Object> statesToUpdate = new HashMap<>();
-		Map<String, Object> statesFromSharedMemory = sharedMemoryStateProvider.getStates();
-		for (Entry<String, Object> entry : statesFromSharedMemory.entrySet()) {
-			String id = entry.getKey();
-			StateConfiguration stateConfiguration = stateProvider.getStateConfiguration(id);
-			if (stateConfiguration != null) {
-				Object newValue = entry.getValue();
-				Object oldValue = stateProvider.getValue(stateConfiguration);
-				if (!Objects.equals(oldValue, newValue)) {
-					statesToUpdate.put(id, newValue);
-					LOGGER.info("Updating " + stateConfiguration + ": " + newValue);
-				}
-			}
-		}
-		StateChangeEvent stateChangeEvent = updateStates(statesToUpdate);
+		StateChangeEvent stateChangeEvent = stateProvider.updateStatesFromSharedMemory();
 		if (stateChangeEvent.getUpdatedStates().isEmpty()) {
 			return;
 		}
 		template.convertAndSend(TOPIC_STATES_UPDATE, stateChangeEvent);
-	}
-
-	private StateChangeEvent updateStates(Map<String, ? extends Object> statesToUpdate) {
-		statesToUpdate.forEach((id, value) -> {
-			StateConfiguration stateConfiguration = stateProvider.getStateConfiguration(id);
-			if (stateConfiguration != null) {
-				stateProvider.put(stateConfiguration, value);
-			}
-		});
-		return new StateChangeEvent(agent, statesToUpdate);
 	}
 
 }
--- viperpit-agent/src/main/java/de/viperpit/agent/controller/StateProvider.java
+++ viperpit-agent/src/main/java/de/viperpit/agent/controller/StateProvider.java
@@ -9,15 +9,20 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import de.viperpit.agent.data.SharedMemoryStateProvider;
+import de.viperpit.agent.model.Agent;
+import de.viperpit.agent.model.StateChangeEvent;
 import de.viperpit.commons.cockpit.StateConfiguration;
 import de.viperpit.commons.cockpit.StateConfigurationStore;
 import de.viperpit.commons.cockpit.StateType;
@@ -27,15 +32,24 @@ public class StateProvider {
 
 	private static final Logger LOGGER = getLogger(StateProvider.class);
 
+	private final Agent agent;
+
 	@Autowired
 	private ObjectMapper objectMapper;
 
 	@Autowired
 	private ResourceLoader resourceLoader;
 
+	@Autowired
+	private SharedMemoryStateProvider sharedMemoryStateProvider;
+
 	private StateConfigurationStore stateConfigurationStore;
 
 	private Map<StateConfiguration, Object> states;
+
+	public StateProvider(@Value("#{environment.COMPUTERNAME}") String agentId) {
+		this.agent = new Agent(agentId);
+	}
 
 	public StateConfiguration getStateConfiguration(String id) {
 		initialize();
@@ -55,12 +69,12 @@ public class StateProvider {
 		}
 	}
 
-	public Collection<Entry<StateConfiguration, Object>> getStates() {
+	private Collection<Entry<StateConfiguration, Object>> getStates() {
 		initialize();
 		return states.entrySet();
 	}
 
-	public Object getValue(StateConfiguration stateConfiguration) {
+	private Object getValue(StateConfiguration stateConfiguration) {
 		initialize();
 		return states.get(stateConfiguration);
 	}
@@ -91,9 +105,66 @@ public class StateProvider {
 		}
 	}
 
-	public void put(StateConfiguration stateConfiguration, Object value) {
+	public StateChangeEvent initializeStates() {
+		Map<StateConfiguration, Object> statesToUpdate = new HashMap<>();
+		for (Entry<StateConfiguration, Object> entry : getStates()) {
+			statesToUpdate.put(entry.getKey(), entry.getValue());
+		}
+		return updateStates(statesToUpdate);
+	}
+
+	private void put(StateConfiguration stateConfiguration, Object value) {
 		initialize();
 		states.put(stateConfiguration, value);
+	}
+
+	public StateChangeEvent toggleBooleanState(StateConfiguration stateConfiguration) {
+		if (stateConfiguration == null || !stateConfiguration.isStateful()) {
+			return null;
+		}
+		Map<StateConfiguration, Object> statesToUpdate = new HashMap<>();
+		Object value = getValue(stateConfiguration);
+		if (value instanceof Boolean) {
+			boolean oldValue = ((Boolean) value).booleanValue();
+			boolean newValue = !oldValue;
+			statesToUpdate.put(stateConfiguration, newValue);
+			for (String idOfRelated : stateConfiguration.getRelatedStateConfigurations()) {
+				var relatedStateConfiguration = getStateConfiguration(idOfRelated);
+				if (relatedStateConfiguration != null) {
+					statesToUpdate.put(relatedStateConfiguration, oldValue);
+				}
+			}
+		}
+		return updateStates(statesToUpdate);
+	}
+
+	private StateChangeEvent updateStates(Map<StateConfiguration, ? extends Object> statesToUpdate) {
+		Map<String, Object> updatedStates = new HashMap<>(statesToUpdate.size());
+		statesToUpdate.forEach((stateConfiguration, value) -> {
+			if (stateConfiguration != null) {
+				put(stateConfiguration, value);
+				updatedStates.put(stateConfiguration.getId(), value);
+			}
+		});
+		return new StateChangeEvent(agent, updatedStates);
+	}
+
+	public StateChangeEvent updateStatesFromSharedMemory() {
+		Map<StateConfiguration, Object> statesToUpdate = new HashMap<>();
+		Map<String, Object> statesFromSharedMemory = sharedMemoryStateProvider.getStates();
+		for (Entry<String, Object> entry : statesFromSharedMemory.entrySet()) {
+			String id = entry.getKey();
+			StateConfiguration stateConfiguration = getStateConfiguration(id);
+			if (stateConfiguration != null) {
+				Object newValue = entry.getValue();
+				Object oldValue = getValue(stateConfiguration);
+				if (!Objects.equals(oldValue, newValue)) {
+					statesToUpdate.put(stateConfiguration, newValue);
+					LOGGER.info("Updating " + stateConfiguration + ": " + newValue);
+				}
+			}
+		}
+		return updateStates(statesToUpdate);
 	}
 
 }
