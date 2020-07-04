c5bbf181bce66517e6509c5c68aaee9c2b717d59
jochenschmich <github@airsupply.de>
2020-07-04 10:37:07 +0200

    Dev server can now be accessed from test machines.

--- viperpit-web/vue.config.js
+++ viperpit-web/vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== "production",
   transpileDependencies: ["vuetify"],
+  devServer: {
+    disableHostCheck: true
+  },
   pluginOptions: {
     express: {
       shouldServeApp: true,
