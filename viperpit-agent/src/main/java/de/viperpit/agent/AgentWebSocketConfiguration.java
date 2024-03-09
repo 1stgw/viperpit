83e3c618407cf4377f697d3ee55cad0c967278db
jochenschmich <github@airsupply.de>
2024-03-09 16:51:18 +0100

    Increased WebSocket timeout

--- viperpit-agent/src/main/java/de/viperpit/agent/AgentWebSocketConfiguration.java
+++ viperpit-agent/src/main/java/de/viperpit/agent/AgentWebSocketConfiguration.java
@@ -1,11 +1,17 @@
 package de.viperpit.agent;
 
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -18,6 +24,20 @@ public class AgentWebSocketConfiguration implements WebSocketMessageBrokerConfig
 		messageBrokerRegistry //
 				.setApplicationDestinationPrefixes("/app") //
 				.enableSimpleBroker("/topic"); //
+	}
+
+	@Override
+	public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+		registry.setTimeToFirstMessage(600_000);
+	}
+
+	@Bean
+	public DefaultHandshakeHandler handshakeHandler() {
+		JettyRequestUpgradeStrategy strategy = new JettyRequestUpgradeStrategy();
+		strategy.addWebSocketConfigurer(configurable -> {
+			configurable.setIdleTimeout(Duration.ofMinutes(10));
+		});
+		return new DefaultHandshakeHandler(strategy);
 	}
 
 	@Override
